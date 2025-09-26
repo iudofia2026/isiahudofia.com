3c1d58d402e36c590642eec88bee30182c92f0f8
Isiah Udofia <isiah.udofia@yale.edu>
2025-09-26 11:53:15 -0400

    chore: clean repo to static portfolio

--- app/loading.tsx
+++
@@ -1,7 +0,0 @@
-export default function Loading() {
-  return (
-    <main className="flex min-h-[50vh] items-center justify-center">
-      <div className="h-12 w-12 animate-spin rounded-full border-2 border-border border-t-accent" aria-label="Loading" />
-    </main>
-  );
-}
--- components/default-seo-config.tsx
+++
@@ -1,8 +0,0 @@
-'use client';
-
-import { DefaultSeo } from 'next-seo';
-import defaultSEO from '../next-seo.config';
-
-export default function DefaultSeoConfig() {
-  return <DefaultSeo {...defaultSEO} />;
-}
